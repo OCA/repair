OCA/manufacture

[ADD] mrp_production_quant_manual_assign

mrp_production_quant_manual_assign/__manifest__.py
@@ -1,0 +1,14 @@
+# Copyright 2021 Quartile Limited
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Production - Manual Quant Assignment",
+    "version": "12.0.1.0.0",
+    "category": "Manufacturing",
+    "license": "AGPL-3",
+    "author": "Quartile Limited, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/manufacturing",
+    "depends": ["mrp", "stock_quant_manual_assign"],
+    "data": ["views/mrp_production_views.xml"],
+    "installable": True,
+}

OCA/repair

repair_calendar_view/mrp_production_quant_manual_assign/__manifest__.py
@@ -1,0 +1,14 @@
+# Copyright 2021 Quartile Limited
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Production - Manual Quant Assignment",
+    "version": "12.0.1.0.0",
+    "category": "Manufacturing",
+    "license": "AGPL-3",
+    "author": "Quartile Limited, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/manufacture",
+    "depends": ["mrp", "stock_quant_manual_assign"],
+    "data": ["views/mrp_production_views.xml"],
+    "installable": True,
+}