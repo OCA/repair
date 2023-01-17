damendieta/manufacture

quality_control: Total refactorization with new API, README files, and new concepts. * Triggers for product category * ACLs * Tolerances in questions * one2many copyable * Fix some views * Error on same tolerance * Colors on inspection lines * Unify inspection header data assignment in set_test method * key name in set_test * Change icon set

quality_control/__openerp__.py
@@ -1,58 +1,52 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-# Copyright (c) 2010 NaN Projectes de Programari Lliure, S.L.
-#                    All Rights Reserved.
-#                    http://www.NaN-tic.com
+# Copyright (c)
+#    2010 NaN Projectes de Programari Lliure, S.L. (http://www.NaN-tic.com)
+#    2014 Serv. Tec. Avanzados - Pedro M. Baeza (http://www.serviciosbaeza.com)
+#    2014 AvanzOsc (http://www.avanzosc.es)
 #
-# WARNING: This program as such is intended to be used by professional
-# programmers who take the whole responsability of assessing all potential
-# consequences resulting from its eventual inadequacies and bugs
-# End users who are looking for a ready-to-use solution with commercial
-# garantees and support are strongly adviced to contract a Free Software
-# Service Company
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
-# This program is Free Software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 {
-    "name": "Quality Control",
-    "version": "0.1",
-    "author": "NaN·tic",
+    "name": "Quality control",
+    "version": "1.0",
+    "author": "OdooMRP team",
+    "website": "http://www.odoomrp.com",
     "contributors": [
+        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com",
         "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+        "Ana Juaristi <ajuaristo@gmail.com>",
     ],
-    "category": "Generic Modules/Others",
-    "summary": "Quality control",
-    "description": """
-This module provides a generic infrastructure for quality tests. The idea is
-that it can be later be reused for doing quality tests in production lots but
-also in any other areas a company may desire.
-
-Developed for Trod y Avia, S.L.""",
+    "category": "Quality control",
     "depends": [
-        'product'
+        'product',
     ],
     "data": [
         'data/quality_control_data.xml',
         'security/quality_control_security.xml',
         'security/ir.model.access.csv',
-        'workflow/test_workflow.xml',
         'wizard/qc_test_wizard_view.xml',
-        'views/quality_control_view.xml',
+        'views/qc_menus.xml',
+        'views/qc_inspection_view.xml',
+        'views/qc_test_category_view.xml',
+        'views/qc_test_view.xml',
+        'views/qc_trigger_view.xml',
+        'views/product_template_view.xml',
+        'views/product_category_view.xml',
     ],
     "installable": True,
 }

OCA/repair

repair_calendar_view/quality_control_oca/__openerp__.py
@@ -1,58 +1,52 @@
 # -*- encoding: utf-8 -*-
 ##############################################################################
 #
-# Copyright (c) 2010 NaN Projectes de Programari Lliure, S.L.
-#                    All Rights Reserved.
-#                    http://www.NaN-tic.com
+# Copyright (c)
+#    2010 NaN Projectes de Programari Lliure, S.L. (http://www.NaN-tic.com)
+#    2014 Serv. Tec. Avanzados - Pedro M. Baeza (http://www.serviciosbaeza.com)
+#    2014 AvanzOsc (http://www.avanzosc.es)
 #
-# WARNING: This program as such is intended to be used by professional
-# programmers who take the whole responsability of assessing all potential
-# consequences resulting from its eventual inadequacies and bugs
-# End users who are looking for a ready-to-use solution with commercial
-# garantees and support are strongly adviced to contract a Free Software
-# Service Company
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
-# This program is Free Software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 {
-    "name": "Quality Control",
-    "version": "0.1",
-    "author": "NaN·tic",
+    "name": "Quality control",
+    "version": "1.0",
+    "author": "OdooMRP team",
+    "website": "http://www.odoomrp.com",
     "contributors": [
+        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com",
         "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
+        "Ana Juaristi <ajuaristio@gmail.com>",
     ],
-    "category": "Generic Modules/Others",
-    "summary": "Quality control",
-    "description": """
-This module provides a generic infrastructure for quality tests. The idea is
-that it can be later be reused for doing quality tests in production lots but
-also in any other areas a company may desire.
-
-Developed for Trod y Avia, S.L.""",
+    "category": "Quality control",
     "depends": [
-        'product'
+        'product',
     ],
     "data": [
         'data/quality_control_data.xml',
         'security/quality_control_security.xml',
         'security/ir.model.access.csv',
-        'workflow/test_workflow.xml',
         'wizard/qc_test_wizard_view.xml',
-        'views/quality_control_view.xml',
+        'views/qc_menus.xml',
+        'views/qc_inspection_view.xml',
+        'views/qc_test_category_view.xml',
+        'views/qc_test_view.xml',
+        'views/qc_trigger_view.xml',
+        'views/product_template_view.xml',
+        'views/product_category_view.xml',
     ],
     "installable": True,
 }