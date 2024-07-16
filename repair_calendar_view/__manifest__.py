531fb1cadcffe4a468479bcae0843b4f4a43bfec
Unai Beristain <unaiberis@gmail.com>
2024-07-16 08:58:58 +0200

    [MIG] repair_calendar_view: Migration to 16.0

--- repair_calendar_view/__manifest__.py
+++ repair_calendar_view/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Repair Calendar View",
-    "version": "16.0.1.1.1",
+    "version": "16.0.1.0.0",
     "category": "Manufacturing",
     "author": "Agile Business Group, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/repair",
