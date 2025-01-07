478ac1efa273600241fa206b19f8270f304022fb
Bhavesh Heliconia <oca@heliconia.in>
2025-01-07 18:01:13 +0530

    [MIG] repair_timesheet: Migration to 18.0

--- repair_timesheet/__manifest__.py
+++ repair_timesheet/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Repair Timesheet",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Repair",
     "website": "https://github.com/OCA/repair",
     "author": "APSL-Nagarro, Odoo Community Association (OCA)",
