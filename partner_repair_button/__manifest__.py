03d5456910f89ef5a9b96f4357e3b6d01ae8abc3
AaronHForgeFlow <aaron.henriquez@forgeflow.com>
2025-08-13 10:04:53 +0200

    [ADD] partner_repair_button

---
+++ partner_repair_button/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Partner Repair Smart Button",
+    "version": "18.0.1.0.0",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/repair",
+    "summary": "Partner Repair Smart Button",
+    "category": "Repair",
+    "depends": ["repair"],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+    "maintainers": ["AaronHForgeFlow"],
+}
