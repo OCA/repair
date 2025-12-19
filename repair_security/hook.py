71e2f5a462bf9af350fc03eeabcc0d622dce82df
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2025-12-19 11:23:01 +0000

    [MIG] repair_security: Migration to 19.0

--- repair_security/hook.py
+++ repair_security/hook.py
@@ -1,12 +1,7 @@
-from odoo import SUPERUSER_ID
-from odoo.api import Environment
-
-
-def post_init_hook(cr, _):
-    env = Environment(cr, SUPERUSER_ID, {})
+def post_init_hook(env):
     # Giving Repair Admin access to match the previous permissions.
     env.ref("repair_security.group_repair_manager").write(
         {
-            "users": [(6, 0, env.ref("stock.group_stock_user").users.ids)],
+            "user_ids": [(6, 0, env.ref("stock.group_stock_user").user_ids.ids)],
         }
     )
