b1d2774e4622814c1d6afa739f18015435a6c5a5
GuillermoNforgeflow <guillermo.navas@forgeflow>
2026-02-23 12:17:04 +0100

    [ADD]repair_stock_move_menu: add menu and action to show repair stock moves

---
+++ repair_stock_move_menu/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Repair Stock Move Menu",
+    "summary": """
+        Adds a menu to obtain a list with repair moves
+    """,
+    "category": "Repair",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/repair",
+    "contributors": [
+        "Guillermo Navas <guillermo.navas@forgeflow.com>",
+    ],
+    "depends": ["repair"],
+    "data": [
+        "views/stock_move_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
---
+++ repair_stock_move_menu/readme/DESCRIPTION.md
@@ -0,0 +1,4 @@
+Adds a menu to list repair moves and create two filters 
+to show consumptions and repaired products separately.
+
+This module depends on `repair`.
