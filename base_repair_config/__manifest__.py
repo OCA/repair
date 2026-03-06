14918d16ef658495c528e491c06263b52dc1b45e
ngabsi <nihel.gabsi@acsone.eu>
2026-03-06 13:04:11 +0100

    [MIG] base_repair_config: Migration to 19.0

--- base_repair_config/__manifest__.py
+++ base_repair_config/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Base Repair Config",
     "summary": """
         Provides general settings for the Repair App""",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "Escodoo,Odoo Community Association (OCA)",
     "maintainers": ["marcelsavegnago"],
